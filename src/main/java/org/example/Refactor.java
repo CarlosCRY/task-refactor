17a69ec3191d2aa90b151216b93a66c6c292a71b
CarlosCRY <trueccr@gmail.com>
2025-12-05 12:05:52 +0100

    Refactorización nominadores variables terminadas.

--- src/main/java/org/example/Refactor.java
+++ src/main/java/org/example/Refactor.java
@@ -8,52 +8,59 @@ public class Refactor {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int Opt;
+        int option;
+        // Refactorizacion: Opt -> option
         do {
             System.out.println("Elige una opción:");
             System.out.println("1. Sumatorio");
             System.out.println("2. Media");
             System.out.println("3. Salir");
-            Opt = scanner.nextInt();
+            option = scanner.nextInt();
             scanner.nextLine();
 
-            if (Opt == 1) {
-                int chisme;
+            if (option == 1) {
+                int n;
+                // Refactorizacion: chisme -> n
                 do {
                     System.out.print("Introduce un entero positivo: ");
-                    chisme = scanner.nextInt();
+                    n = scanner.nextInt();
                     scanner.nextLine();
-                } while (chisme <= 0);
-
-                int La_suma = 0;
-                for (int i = 1; i <= chisme; i++) {
-                    La_suma += i;
-                }
-
-                System.out.println("El sumatorio es " + La_suma);
-            } else if (Opt == 2) {
-                int _zzzz;
-                do {
-                    System.out.print("Introduce un entero positivo: ");
-                    _zzzz = scanner.nextInt();
-                    scanner.nextLine();
-                } while (_zzzz <= 0);
+                } while (n <= 0);
 
                 int sum = 0;
-                for (int cosa = 0; cosa < _zzzz; cosa++) {
+                // Refactorización: La_suma -> sum
+                for (int i = 1; i <= n; i++) {
+                    sum += i;
+                }
+
+                System.out.println("El sumatorio es " + sum);
+            } else if (option == 2) {
+                int amount;
+                // Refactorizacion: -zzzz -> amount
+                do {
+                    System.out.print("Introduce un entero positivo: ");
+                    amount = scanner.nextInt();
+                    scanner.nextLine();
+                } while (amount <= 0);
+
+                int sum = 0;
+                // Nota: podriamos cambiar "sum" debido a que existe una variable tipada con
+                // el mismo nominador. Pero existen en diferentes ambitos, por lo que no es necesario.
+                for (int i = 0; i < amount; i++) {
+                    // Refactorizacion: cosa -> i
                     System.out.print("Introduce un número:");
                     sum += scanner.nextInt();
                     scanner.nextLine();
                 }
 
-                System.out.println("La media es " + (sum / _zzzz));
-            } else if (Opt == 3) {
+                System.out.println("La media es " + (sum / amount));
+            } else if (option == 3) {
                 System.out.println("Saliendo...");
             } else {
                 System.out.println("Opción inválida");
             }
 
-        } while (Opt != 3);
+        } while (option != 3);
     }
 }
 // Codigo formateado
